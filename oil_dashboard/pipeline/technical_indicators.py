c341c1ac70867139bf3ee7b4ba212a082412c559
mrajancsr <mrajancsr@gmail.com>
2025-03-02 23:04:51 -0500

    added moving average function

--- oil_dashboard/pipeline/technical_indicators.py
+++ oil_dashboard/pipeline/technical_indicators.py
@@ -1,4 +1,45 @@
+from typing import List
+
 import pandas as pd
+
+
+def calculate_moving_average(
+    df: pd.DataFrame, column: str, windows: List[int] = [50, 200]
+) -> pd.DataFrame:
+    """Calculates moving average for a given column
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing price data
+    column : str
+        Column name to calculate MAs for
+    windows : List[int], optional, default [50, 100]
+        List of moving average windows to calculate
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame with additional columns for each moving average
+
+    Raises
+    ------
+    KeyError
+        if the particular column is not found
+    """
+    if column not in df:
+        raise KeyError(f"Column {column} not in dataframe")
+
+    for window in windows:
+        df[f"{column}_MA{window}"] = df[column].rolling(window).mean()
+
+    return df
+
+
+def calculate_bollinger_bands(
+    df: pd.DataFrame, column: str, window: int = 20
+) -> pd.DataFrame:
+    pass
 
 
 def add_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
